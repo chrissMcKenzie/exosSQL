3e73d0dfa29fb873f211f18a09ffd916ef92a75f
chrissMcKenzie <chrissMcKenzie.IT.Agence@gmail.com>
2022-12-06 15:36:08 +0100

    Exam: SQL

--- SQL1.1-02-Requêtes-SELECT-simples.sql
+++ SQL1.1-02-Requêtes-SELECT-simples.sql
@@ -1,4 +1,5 @@
 /* La liste des bureaux (adresse et ville) triés par pays décroissant puis par état */
-
+SELECT addressLine1, addressLine2, city FROM offices ORDER BY country DESC;
+SELECT addressLine1, addressLine2, city FROM offices ORDER BY offices.state DESC;
 
 /* RESULTAT ==> 7 lignes / 100 Market Street */
--- SQL1.1-04-Colonnes-calculées.sql
+++ SQL1.1-04-Colonnes-calculées.sql
@@ -1,13 +1,19 @@
 /* La liste des motos (nom, vendeur, quantité et marge) triés par marge décroissante */
-
+SELECT productName, productVendor, quantityInStock, (MSRP - buyPrice) AS Marge FROM products
+WHERE productLine = "Motorcycles"
+ORDER BY Marge DESC;
 
 /* RESULTAT ==> 13 lignes / 2003 Harley-Davidson Eagle Drag Bike */
 
 
 
 /* La liste des commandes (numéro, date de commande, date d'expédition, écart en jours entre les deux dates et statut) qui sont en cours de traitement ou qui ont été expédiées et ont un écart de plus de 10j triés par écart décroissant puis par date de commande */
+SELECT orderNumber, orderDate, shippedDate, DATEDIFF(shippedDate, orderDate), orders.status FROM orders
+WHERE (DATEDIFF(shippedDate, orderDate) > 10 AND status = "Shipped" OR status = "In Pocess")
+-- ORDER BY DATEDIFF(shippedDate, orderDate) DESC, orderDate DESC;
 
 
+	, , requiredDate, shippedDate, 
 /* RESULTAT ==> 33 lignes / 10165 */
 
 
